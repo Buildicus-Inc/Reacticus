cf632d392d89e0f7b7e57a0d9898dd9b24d26f59
Zach <zackify@gmail.com>
2014-10-30 14:29:46 -0700

    added concat

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
 var gulp = require('gulp');
 var react = require('gulp-react');
-
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
 gulp.task('default', function () {
     return gulp.src('lib/**')
+    		.pipe(concat('reacticus.js'))
         .pipe(react())
-        .pipe(uglify())
+        //.pipe(uglify())
         .pipe(gulp.dest('dist'));
 });
