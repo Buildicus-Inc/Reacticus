66c8c7a37d8103febaf3360f7440bf021893b8ce
Zach <zackify@gmail.com>
2014-10-30 14:12:10 -0700

    added bower and moved around

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,8 @@ var gulp = require('gulp');
 var react = require('gulp-react');
 
 gulp.task('default', function () {
-    return gulp.src('elements/**')
+    return gulp.src('lib/**')
         .pipe(react())
+        .pipe(uglify())
         .pipe(gulp.dest('dist'));
 });
